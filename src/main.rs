6a1931765bec179dcad4665c4f8858524fc70959
Kristian Monsen Haug <kristian.haug95@gmail.com>
2021-02-19 19:34:43 +0100

    Handle simple keyboard input

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::time::Duration;
 use std::io::{self,Read};
 
 extern crate sdl2;
-use sdl2::pixels::Color;
+use sdl2::{keyboard, pixels::Color};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
@@ -17,9 +17,9 @@ const HEIGHT: u32 = 32 * SCALE;
 
 pub fn main() {
     let mut machine = chip8::Machine::new();
-    // let mut file = std::fs::File::open("IBM_Logo.ch8").unwrap();
+    let mut file = std::fs::File::open("IBM_Logo.ch8").unwrap();
     // let mut file = std::fs::File::open("test_opcode.ch8").unwrap();
-    let mut file = std::fs::File::open("BC_test.ch8").unwrap();
+    //let mut file = std::fs::File::open("BC_test.ch8").unwrap();
 
     let mut buf = Vec::new();
     file.read_to_end(&mut buf).unwrap();
@@ -44,6 +44,7 @@ pub fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     'running: loop {
+
         let instruction:u16 = ((machine.memory[machine.program_counter as usize] as u16) << 8)
                             | machine.memory[machine.program_counter as usize + 1] as u16;
 
@@ -61,6 +62,54 @@ pub fn main() {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
+                Event::KeyDown { keycode: Some(kcode), ..} => {                        
+                    match kcode {
+                        Keycode::Num1 => machine.keyboard[0x1] = true,
+                        Keycode::Num2 => machine.keyboard[0x2] = true,
+                        Keycode::Num3 => machine.keyboard[0x3] = true,
+                        Keycode::Num4 => machine.keyboard[0xC] = true,
+
+                        Keycode::Q    => machine.keyboard[0x4] = true,
+                        Keycode::W    => machine.keyboard[0x5] = true,
+                        Keycode::E    => machine.keyboard[0x6] = true,
+                        Keycode::R    => machine.keyboard[0xD] = true,
+
+                        Keycode::A    => machine.keyboard[0x7] = true,
+                        Keycode::S    => machine.keyboard[0x8] = true,
+                        Keycode::D    => machine.keyboard[0x9] = true,
+                        Keycode::F    => machine.keyboard[0xE] = true,
+
+                        Keycode::Z    => machine.keyboard[0xA] = true,
+                        Keycode::X    => machine.keyboard[0x0] = true,
+                        Keycode::C    => machine.keyboard[0xB] = true,
+                        Keycode::V    => machine.keyboard[0xF] = true,
+                        _ => {},
+                    }
+                },
+                Event::KeyUp { keycode: Some(kcode), ..} => {                        
+                    match kcode {
+                        Keycode::Num1 => machine.keyboard[0x1] = false,
+                        Keycode::Num2 => machine.keyboard[0x2] = false,
+                        Keycode::Num3 => machine.keyboard[0x3] = false,
+                        Keycode::Num4 => machine.keyboard[0xC] = false,
+
+                        Keycode::Q    => machine.keyboard[0x4] = false,
+                        Keycode::W    => machine.keyboard[0x5] = false,
+                        Keycode::E    => machine.keyboard[0x6] = false,
+                        Keycode::R    => machine.keyboard[0xD] = false,
+
+                        Keycode::A    => machine.keyboard[0x7] = false,
+                        Keycode::S    => machine.keyboard[0x8] = false,
+                        Keycode::D    => machine.keyboard[0x9] = false,
+                        Keycode::F    => machine.keyboard[0xE] = false,
+
+                        Keycode::Z    => machine.keyboard[0xA] = false,
+                        Keycode::X    => machine.keyboard[0x0] = false,
+                        Keycode::C    => machine.keyboard[0xB] = false,
+                        Keycode::V    => machine.keyboard[0xF] = false,
+                        _ => {},
+                    }
+                }
                 _ => {}
             }
         }
